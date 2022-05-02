f2fff37e92dbc168ab28ad126a60b3bf32fb2115
Annu <annupoddar444@gmail.com>
2022-05-02 22:00:43 +0530

    Hint Catagory

---
+++ api.js
@@ -0,0 +1,16 @@
+const category = [
+    {
+        0 :['Animal', 'MONKEY', 'WOLF', 'TIGER', 'LION']
+
+    },
+
+    {
+        1 : ['Bird','PARROT', 'CROW', 'PICOCK']
+    },
+
+    {
+        2 : ['Flower', 'ROSE', 'LILI', 'JASMIN']
+    }
+
+
+]
